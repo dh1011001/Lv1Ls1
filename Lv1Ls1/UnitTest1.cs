f4b2626d2f671b718a8e46c70b5fc88aed1c749a
dh1011001 <62588322+dh1011001@users.noreply.github.com>
2022-12-15 01:39:14 +0300

    Update UnitTest1.cs

--- Lv1Ls1/UnitTest1.cs
+++ Lv1Ls1/UnitTest1.cs
@@ -1,42 +1,162 @@
 using NUnit.Framework;
+using Newtonsoft.Json.Linq;
+using YamlDotNet.Serialization;
+using System.IO;
+using System;
 
 namespace Lv1Ls1
 {
+
     [TestFixture]
     public class UnitTest1
     {
-        List<User> adminsJSON = new List<User>();
-        List<User> adminsYAML = new List<User>();
-        List<User> regularsJSON = new List<User>();
-        List<User> regularsYAML = new List<User>();
+        JArray arrJsonAdmin = new JArray();
+        JArray arrJsonReg = new JArray();
+        JArray arrYamlAdmin = new JArray();
+        JArray arrYamlReg = new JArray();
 
         [SetUp]
         public void getdata()
         {
-            //Набиваем соответствующие массивы из файлов
+            var exePath = AppDomain.CurrentDomain.BaseDirectory; //РЎСЃС‹Р»РєР° РЅР° РєРѕСЂРЅРµРІСѓСЋ РїР°РїРєСѓ РїСЂРѕРµРєС‚Р°
+            
+            arrJsonAdmin = getArrFromJSON(Path.Combine(exePath, "DataSet\\admin_users.json"));
+            arrJsonReg = getArrFromJSON(Path.Combine(exePath, "DataSet\\regular_users.json"));
+            arrYamlAdmin = getArrFromYAML(Path.Combine(exePath, "DataSet\\admin_users.yaml"));
+            arrYamlReg = getArrFromYAML(Path.Combine(exePath, "DataSet\\regular_users.yaml"));
         }
 
 
-        [TestCase]
-        [TestCase]
-        [TestCase]
-        [TestCase]
-        public void IsUsers()
+        [Test]
+        public void IsUsersInJSON()
         {
-           //Проверяем корректность данных, наличие id и name
+            bool isUsersInJSON = true;
+            foreach (JObject jObject in arrJsonAdmin)
+            {
+                if (jObject.ContainsKey("id") && jObject.ContainsKey("name")) continue;
+                else isUsersInJSON = false;
+            }
+
+            foreach (JObject jObject in arrJsonReg)
+            {
+                if (jObject.ContainsKey("id") && jObject.ContainsKey("name")) continue;
+                else isUsersInJSON = false;
+            }
+
+            Assert.IsTrue(isUsersInJSON);
         }
 
-        [TestCase]
-        [TestCase]
-        public void IsFilesEq()
+        [Test]
+        public void IsUsersInYAML()
         {
-            //Стравниваем соответствующме элементы массивов
+            bool isUsersInJSON = true;
+            foreach (JObject jObject in arrYamlAdmin)
+            {
+                if (jObject.ContainsKey("id") && jObject.ContainsKey("name")) continue;
+                else isUsersInJSON = false;
+            }
+
+            foreach (JObject jObject in arrYamlReg)
+            {
+                if (jObject.ContainsKey("id") && jObject.ContainsKey("name")) continue;
+                else isUsersInJSON = false;
+            }
+
+            Assert.IsTrue(isUsersInJSON);
+        }
+
+        [Test]
+        public void IsJSONUsersIncludInYAML()
+        {
+            int countOfSameUsersAdmin = 0;
+            int countOfSameUsersReg = 0;
+            bool isUserAreSame = false;
+
+            foreach (JObject JASONjObject in arrJsonAdmin)
+            {
+                foreach(JObject YAMLjObject in arrYamlAdmin)
+                {
+                    if ((int)JASONjObject["id"] == (int)YAMLjObject["id"])
+                    {
+                        countOfSameUsersAdmin++;
+                        break;
+                    }
+                }
+            }
+
+            foreach (JObject JASONjObject in arrJsonReg)
+            {
+                foreach (JObject YAMLjObject in arrYamlReg)
+                {
+                    if ((int)JASONjObject["id"] == (int)YAMLjObject["id"])
+                    {
+                        countOfSameUsersReg++;
+                        break;
+                    }
+                }
+            }
+
+            if (arrJsonAdmin.Count == countOfSameUsersAdmin && arrJsonReg.Count == countOfSameUsersReg) 
+                isUserAreSame = true;
+
+            Assert.IsTrue(isUserAreSame);
+        }
+
+        [TearDown]
+        public void teardoun()
+        {
+            arrJsonAdmin.Clear();
+            arrJsonReg.Clear();
+            arrYamlAdmin.Clear();
+            arrYamlReg.Clear();
+        }
+
+
+
+
+        //РњРµС‚РѕРґ РїСЂРµРѕР±СЂР°Р·РѕРІР°РЅРёСЏ С„Р°Р№Р»Р° JSON РІ РјР°СЃСЃРёРІ JArray
+        public JArray getArrFromJSON(string path)
+        {
+            string strJsonFromFile;
+            JArray jArray = new JArray();
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                strJsonFromFile = reader.ReadToEnd();
+            }
+
+            jArray = JArray.Parse(strJsonFromFile);
+
+
+            return jArray;
+        }
+
+        //РњРµС‚РѕРґ РїСЂРµРѕР±СЂР°Р·РѕРІР°РЅРёСЏ С„Р°Р№Р»Р° YAML РІ РјР°СЃСЃРёРІ JArray
+        public JArray getArrFromYAML(string path)
+        {
+            string strYamlFromFile;
+            string finStrYaml;
+            JArray jArray = new JArray();
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                strYamlFromFile = reader.ReadToEnd();
+            }
+
+            var _reader = new StringReader(strYamlFromFile);
+            var _writer = new StringWriter();
+
+            var deserializer = new Deserializer();
+            var yamlObject = deserializer.Deserialize(_reader);
+
+            var serializer = new Serializer(SerializationOptions.JsonCompatible);
+            serializer.Serialize(_writer, yamlObject);
+            finStrYaml = _writer.ToString();
+
+            jArray = JArray.Parse(finStrYaml);
+
+            return jArray;
         }
     }
 
-    public class User
-    {
-        public int Id;
-        public string Name;
-    }
-}   
+}
